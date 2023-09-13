8328159183a5809e2221b270a6dee17e7551f73c
0xhardman <1142783160@qq.com>
2023-09-14 00:50:39 +0800

    complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,65 @@
-import { useAccount, useNetwork, usePrepareContractWrite, useSwitchNetwork } from 'wagmi'
-import { useEffect, useState, useMemo } from 'react'
+/* eslint-disable @next/next/no-img-element */
+import { useNetwork } from 'wagmi'
+import { useState, useMemo } from 'react'
 import clsx from 'clsx'
 
 import { useContractWrite, erc20ABI } from 'wagmi';
 import {
-  AxelarQueryAPI,
   AxelarAssetTransfer,
   CHAINS,
   Environment,
 } from "@axelar-network/axelarjs-sdk";
 
-import { useMetaMask } from '@/utils/hooks/useMetaMask';
+import { useMetaMask } from '@/utils/hooks/useMetamask';
+import { useRouter } from 'next/router';
 
+interface Chain {
+  name: string;
+  icon: string;
+  axelarName: string;
 
-const sdk = new AxelarAssetTransfer({
+}
+
+interface ChainList {
+  [key: string]: Chain;
+}
+
+const config = {
   environment: Environment.TESTNET
-});
+}
 
-const DonateBtn = ({ donateTo, amount, source, destination }: { donateTo: `0x${string}`, amount: string, source: any, destination: any }) => {
+
+
+const coinType: ChainList = {
+  '80001': {
+    name: 'Polygon Mumbai',
+    icon: '/icons/support/polygon.svg',
+    axelarName: 'Polygon'
+
+  },
+  '5': {
+    name: 'ETH Goerli',
+    icon: '/icons/support/ethereum.svg',
+    axelarName: 'ethereum-2'
+  },
+  '59140': {
+    name: 'Linea Goerli',
+    icon: '/icons/support/linea.svg',
+    axelarName: 'linea'
+  },
+  '420': {
+    name: 'Op Goerli',
+    icon: '/icons/support/optimism.svg',
+    axelarName: 'optimism'
+  },
+};
+
+
+const DonateBtn = ({ donateTo, amount, toChain }:
+  { donateTo: `0x${string}`, amount: number, toChain: string }) => {
   const { wallet, hasProvider, connectMetaMask } = useMetaMask();
-  const { address } = useAccount()
-  const { chain } = useNetwork();
-  const { chains, isLoading: swichLoading, switchNetwork } = useSwitchNetwork();
-
+  const { chain } = useNetwork()
+  const a = chain?.id || 5
 
   const { data, isLoading, isSuccess, writeAsync } = useContractWrite({
     address: '0x254d06f33bDc5b8ee05b2ea472107E300226659A',
@@ -32,75 +69,56 @@ const DonateBtn = ({ donateTo, amount, source, destination }: { donateTo: `0x${s
   })
 
   const handleDonate = async () => {
-    console.log(123)
-    const fromChain = CHAINS.TESTNET.ETHEREUM,
-      toChain = CHAINS.TESTNET.OPTIMISM,
-      destinationAddress = donateTo,
+    const destinationAddress = donateTo,
       asset = "uausdc";  // denom of asset. See note (2) below
+    const sdk = new AxelarAssetTransfer(config);
 
     const depositAddress = await sdk.getDepositAddress({
-      fromChain,
-      toChain,
+      fromChain: coinType[a].axelarName || CHAINS.TESTNET.ETHEREUM,
+      toChain: toChain,
       destinationAddress,
       asset
     });
-    console.log("this is deposit: ", depositAddress)
 
     writeAsync({
       args: [
         depositAddress as `0x${string}`,
-        BigInt(Math.floor(parseFloat(amount)) * 1e6 || 10e6)
+        BigInt(Math.floor(amount) * 1e6 || 10e6)
       ]
     })
-
-
   }
 
-  const memoBtnText = useMemo(() => {
-    if (!hasProvider) {
-      return '';
-    }
-    if (wallet?.accounts?.length > 0) {
-      // return `${account.slice(0, 4)}...${account.slice(-5)}`;
-      return `Support`;
-    }
-    return 'Connect MetaMask';
-  }, [wallet, hasProvider]);
-
-  const memoBtnAction = useMemo(() => {
-    if (!hasProvider) {
-      return () => { };
-    }
-    if (wallet?.accounts?.length > 0) {
-      // return `${account.slice(0, 4)}...${account.slice(-5)}`;
-      return handleDonate;
-    }
-    return () => { };
-  }, [wallet, hasProvider]);
-
   return (
-    <div className='rounded-full bg-[#d0fb51] h-[40px] w-full text-center font-bold text-white leading-[40px] text-[24px] cursor-pointer' onClick={memoBtnAction}>
-      {memoBtnText}
+    <div className={clsx(amount == 0 ? 'cursor-not-allowed bg-[#9dbd3c]' : 'cursor-pointer bg-[#d0fb51]', 'rounded-full h-[40px] w-full text-center font-bold text-white leading-[40px] text-[24px]')} onClick={wallet?.accounts?.length > 0 ? handleDonate : connectMetaMask}>
+      {wallet?.accounts?.length > 0 ? `Support` : 'Connect MetaMask'}
     </div>
   );
 };
 
 export default function Home() {
-  const { address } = useAccount()
   const [donation, setDonation] = useState(0)
-  console.log(address)
+  const { query } = useRouter()
+  const { chain } = useNetwork()
+
   return (
     <main
       className="w-screen h-screen flex flex-col justify-center items-center"
     >
       <div className='w-[350px]  px-[20px] py-[30px] flex flex-col gap-[15px] justify-center items-center border border-gray-300 rounded-lg'>
-        <div className='text-[24px] self-start font-semibold'>Donate <span className='text-[#717171]'>0xhardman</span> aUSDC</div>
+        <div className='text-[24px] self-start font-semibold'>Donate <span className='text-[#717171]'>{query?.name}</span> aUSDC</div>
+        <div className='flex gap-4 w-full items-center'>
+          <div className='flex justify-center items-center gap-3 border  border-[#d0fb51] text-[12px] rounded-md w-[calc(50vw_-_240px)] h-[40px] p-2'>
+            <img className='w-[30px] h-[30px] bg-[#132333] rounded-full' src={coinType[chain?.id + '']?.icon} alt="" />{coinType[chain?.id + '']?.name}
+          </div>
+          <img width="20px" src="./ar.png" alt="" />
+          <div className='flex justify-center items-center gap-3 border  border-[#d0fb51] text-[12px] rounded-md w-[calc(50vw_-_240px)] h-[40px] p-2'>
+            <img className='w-[30px] h-[30px] bg-[#132333] rounded-full' src={coinType[query.toChain as string]?.icon} alt="" />{coinType[query.toChain as string]?.name}</div>
+        </div>
         <div className='w-full h-[60px] flex justify-center items-center rounded-lg border border-[#d0fb51] bg-[#d0fb5166] gap-4'>
           <div className='text-[50px]'>💵</div>
           x
           {[1, 3, 5].map((v, i) => {
             return <div onClick={() => {
-              console.log(v)
               setDonation(v)
             }} key={i} className={clsx(
               donation == v ? 'bg-[#d0fb51] text-white' : 'bg-white text-[#d0fb51]',
@@ -113,8 +131,10 @@ export default function Home() {
             onChange={(e: any) => setDonation(e.target.value)}
           ></input>
         </div>
-        {/* <div className='rounded-full bg-[#d0fb51] h-[40px] w-full text-center font-bold text-white leading-[40px] text-[24px]'>Support</div> */}
-        <DonateBtn donateTo='0xb15115A15d5992A756D003AE74C0b832918fAb75' amount={"1"} source={undefined} destination={undefined} />
+        <DonateBtn donateTo='0xb15115A15d5992A756D003AE74C0b832918fAb75' amount={donation} toChain={query.toChain as string} />
+        <div className='w-full h-[60px] p-2 flex justify-center items-center rounded-lg border border-[#d0fb51] text-[#91ae39] bg-[#d0fb5166] gap-4'>
+          Only supports mubai(80001), goerli(5), linea(59140), and op goerli(420).
+        </div>
       </div>
     </main>
   )
